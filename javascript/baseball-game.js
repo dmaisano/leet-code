5ecf5bf8c1822d9fddb00d0e947dc542e9bc4069
Domenico Maisano <dev.dmaisano@gmail.com>
2017-11-30 14:42:44 -0500

    finished baseball-game.js

--- javascript/baseball-game.js
+++ javascript/baseball-game.js
@@ -1,13 +1,37 @@
 let calPoints = (ops) => {
+  let scores = [];
+  let count = 0;
   let sum = 0;
 
-  for(let i = 0; i <= ops.length; i++) {
+  for(i in ops) {
     if(!isNaN(ops[i])) {
-      sum += Number(ops[i]);
-      console.log("Round " + ++i + " You could get " + ops[i] + " points. The sum is: " + sum);
+      scores.push(Number(ops[i]));
+    }
+
+    if(ops[i] === '+' && scores.length >= 2) {
+      scores.push(scores[scores.length-1] + scores[scores.length-2]);
+    }
+    
+    else if(ops[i] === '+' && scores.length < 2) {
+      console.log('Invalid Operation, need two valid scores to get the sum!');
+    }
+
+    if(ops[i] === 'D') {
+      scores.push(scores[scores.length-1] * 2);
+    }
+
+    if(ops[i] === 'C') {
+      scores.pop();
     }
   }
+
+  for(score of scores) {
+    sum += score;
+    console.log(`Round ${++count}: You could get ${score} points. The sum is: ${sum}`);
+  }
+
+  return sum;
 };
 
-let test = ["5","2"];
+let test = ['5', '2', 'C', 'D', '+'];
 calPoints(test);
