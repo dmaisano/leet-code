6ef3e1c77665175befccb26fed275a2eb38aed57
dmaisano <dmaisanooo@gmail.com>
2020-08-29 21:54:43 -0400

    Added longest-palindromic-substring problem

---
+++ c++/medium/longest-palindromic-substring.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+    }
+};
+
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
