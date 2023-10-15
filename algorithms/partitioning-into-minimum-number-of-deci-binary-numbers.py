5788b71ad5125d4d51b5e6fbd5ec1ceeb40db227
Dom Maisano <dmaisanooo@gmail.com>
2023-10-15 16:44:06 -0400

    chore: add solutions

---
+++ algorithms/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+  def minPartitions(self, n: str) -> int:
+    max_digit = max(map(int, n))
+    return max_digit
+
+
+
+if __name__ == "__main__":
+  res = Solution().min_deci_binary_numbers("27346209830709182346")
+  print(res)
