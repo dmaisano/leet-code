343728a1cd47490955ec1ef265099a56cf060af0
dmaisano <dmaisanooo@gmail.com>
2024-08-05 22:51:00 -0400

    chore: added robot problem

---
+++ algorithms/robot_return_to_origin_test.py
@@ -0,0 +1,19 @@
+import pytest
+from .robot_return_to_origin import Solution
+
+
+@pytest.fixture
+def soln() -> Solution:
+    return Solution()
+
+
+def test_judgeCircle(soln: Solution) -> None:
+    assert soln.judgeCircle("UD") == True, "Test case 1 failed"
+    assert soln.judgeCircle("LL") == False, "Test case 2 failed"
+    assert soln.judgeCircle("") == True, "Test case 3 failed"
+    assert soln.judgeCircle("LRUD") == True, "Test case 4 failed"
+    assert soln.judgeCircle("RRDDLUU") == False, "Test case 5 failed"
+
+
+if __name__ == "__main__":
+    pytest.main()
